49ecc177d49e7a179f2551110ef84fb2a33e5857
Ruwaidah <ruwaidah.riyadh@gmail.com>
2023-04-22 01:31:44 -0500

    answering all the Questions

---
+++ VariablesAndDataTypes.js
@@ -0,0 +1,28 @@
+// 1- Create the following variables
+
+// name, which is a string set to your current name
+let name = "Ruwaidah";
+
+// dayOfBirth, which is a number set to the day of your birth month
+let dayOfBirth = "02";
+
+// 2- See what happens when you have multiple variables of the same name. Which one takes precedence?
+   // let name = "Alfakhri"   
+   // Error we can't declare two variables with the same name
+
+
+// 3- Write some JavaScript that prompt’s the user for their favorite color. Once the user has submitted a favorite color, log that color to the console along with a friendly message.
+let favotiteColor = prompt("what is your favorite color?");
+console.log(`your favorite color is ${favotiteColor}`);
+
+// 4-Create a string that contains both single quotes and double quotes.
+const string = "Hello, 'I love to learn new things all the time'."
+
+// 5- What is the difference between null and undefined?
+   //  we can declare a variable with null value but undefined is something we get it from JS as an answer.
+
+// 6- What is NaN in JavaScript? What is the typeof NaN?
+    // NaN mean is not a number typeof NaN is number
+
+// 7- You can declare a variable by typing let thing;. What is the value of thing?
+    // its undefined
